6072f211d1b13fe1135aa4204ad1a30b3c22e60f
Chitransh <chaudharichitransh9@gmail.com>
2025-04-02 12:53:54 +0530

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string= Bolt-Kernel By FACKJONNY
+kernel.string= Leaf-Kernel By FACKJONNY
 do.devicecheck=1
 do.modules=0
 do.systemless=1
