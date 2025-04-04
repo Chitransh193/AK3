9722bf5b550bc530ed8f30912bdeee247d85452c
Chitransh <chaudharichitransh9@gmail.com>
2025-04-04 20:18:53 +0530

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string= Leaf-Kernel By FACKJONNY
+kernel.string= Stormbreaker-Kernel By FACKJONNY
 do.devicecheck=1
 do.modules=0
 do.systemless=1
